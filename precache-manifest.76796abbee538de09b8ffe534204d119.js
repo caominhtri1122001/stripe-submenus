9f0602bb660bb162ea4e81ccb29fc49beb380da2
Cao Minh Tri <caominhtri112201@gmail.com>
2022-06-02 00:09:29 +0700

    Updates

---
+++ precache-manifest.76796abbee538de09b8ffe534204d119.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea4ba806eac51e7e1ca9eb5dd016fc5",
+    "url": "/stripe-submenus/index.html"
+  },
+  {
+    "revision": "675f117bb6a36bb1ce0f",
+    "url": "/stripe-submenus/static/css/main.12d4bfb7.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-submenus/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-submenus/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675f117bb6a36bb1ce0f",
+    "url": "/stripe-submenus/static/js/main.06600679.chunk.js"
+  },
+  {
+    "revision": "fb1405f2e1e392e6e32d",
+    "url": "/stripe-submenus/static/js/runtime-main.a65415c3.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe-submenus/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe-submenus/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe-submenus/static/media/phone.58d7e3d6.svg"
+  }
+]);
