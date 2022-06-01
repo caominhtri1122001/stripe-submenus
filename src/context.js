5789708e379a776621f2af97585efd66951cab4c
Cao Minh Tri <caominhtri112201@gmail.com>
2022-06-02 00:05:26 +0700

    use context example

--- src/context.js
+++ src/context.js
@@ -5,9 +5,10 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 export const AppProvider = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
-
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({page:'',links:[]});
 
     const openSidebar = () => {
         setIsSidebarOpen(true);
@@ -17,7 +18,10 @@ export const AppProvider = ({children}) => {
         setIsSidebarOpen(false);
     }
 
-    const openSubmenu = () => {
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page);
+        setLocation(coordinates);
         setIsSubmenuOpen(true);
     }
 
@@ -32,7 +36,9 @@ export const AppProvider = ({children}) => {
         openSubmenu,
         openSidebar,
         closeSubmenu,
-        closeSidebar
+        closeSidebar,
+        location,
+        page,
     }}>
         {children}
     </AppContext.Provider>
